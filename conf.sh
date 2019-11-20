289722b645b4eab882f5c64fb4f3d3e48390d078
kyonRay <kyonGuo@163.com>
2019-11-20 14:38:40 +0000

    local test

--- conf.sh
+++ conf.sh
@@ -1,14 +1,14 @@
 base="$(dirname ${BASH_SOURCE[0]})"
 scripts_base="$base/exp"
 src="./libhotstuff" # the source code directory (local)
-proj_path="/home/ubuntu/hs-ec2-example/libhotstuff" # the source code directory (remote)
+proj_path="/libhotstuff_test_env/libhotstuff" # the source code directory (remote)
 proj_server_bin="examples/hotstuff-app"
 proj_server_path="$proj_path/$proj_server_bin"
 proj_client_bin="examples/hotstuff-client"
 proj_client_path="$proj_path/$proj_client_bin"
 proj_conf_name="hotstuff.conf"
 remote_base="/home/ubuntu/hs-ec2-example/testbed"
-ec2_user=ubuntu
+ec2_user=root
 #ec2_profile=ia
 #ec2_keyid="ted-aws-key"
 #ec2_ami="ami-03a29238640f23d42"
--- run_ec2.sh
+++ run_ec2.sh
@@ -7,10 +7,10 @@ source "$base/conf.sh"
 
 peer_list="./nodes.txt"
 server_map="$base/ec2_server_map.txt"
-copy_to_remote_pat="rsync -e 'ssh -o UserKnownHostsFile=$keyscan -i $ec2_pem' -avz <local_path> <remote_user>@<remote_ip>:<remote_path>"
-copy_from_remote_pat="rsync -e 'ssh -o UserKnownHostsFile=$keyscan -i $ec2_pem' -avz <remote_user>@<remote_ip>:<remote_path> <local_path>"
+copy_to_remote_pat="rsync -e 'ssh -o UserKnownHostsFile=$keyscan -avz <local_path> <remote_user>@<remote_ip>:<remote_path>"
+copy_from_remote_pat="rsync -e 'ssh -o UserKnownHostsFile=$keyscan -avz <remote_user>@<remote_ip>:<remote_path> <local_path>"
 run_remote_pat="$run_server_remote_pat"
-exe_remote_pat="ssh -o UserKnownHostsFile=$keyscan -i '$ec2_pem' <remote_user>@<remote_ip> bash"
+exe_remote_pat="ssh -o UserKnownHostsFile=$keyscan <remote_user>@<remote_ip> bash"
 
 run="$scripts_base/run.sh"
 
